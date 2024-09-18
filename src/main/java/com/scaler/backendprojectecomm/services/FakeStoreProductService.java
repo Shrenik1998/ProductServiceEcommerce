d0d2a3ce335174f91977f6f9aad0f1e5c46dba02
Shrenik <shrenikhalyal98@gmail.com>
2024-09-18 10:11:34 +0530

    added comments

--- src/main/java/com/scaler/backendprojectecomm/services/FakeStoreProductService.java
+++ src/main/java/com/scaler/backendprojectecomm/services/FakeStoreProductService.java
@@ -16,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Service//proxy for fakestore.com apis
 public class FakeStoreProductService implements ProductService{
 
     private RestTemplate restTemplate;
